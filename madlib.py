49bcbd4bde03e2978877ac6a7cf4c7de2ba48f58
AZBLACKPHOENIX <unlimitednigami@gmail.com>
2024-07-19 21:42:03 +0200

    Simple_Madlib_Game

---
+++ madlib.py
@@ -0,0 +1,8 @@
+me = input("Name:")
+age = input("Age:")
+adj =input("Adjective:")
+prog_lan = input("What's Your favourite Programming_Langauge:")
+carrer=input("Carrer:")
+
+madlib=("My name is " +name + " I am " +  age + " old " +  "programming is " + adj + " my favourite programming language is " + prog_lan + ", my dream job is "+carrer)
+print(madlib)
